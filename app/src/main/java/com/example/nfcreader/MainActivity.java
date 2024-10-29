a12063a062f6b18183a338d7d97e50d75c6889a7
sharavana07 <140256712+sharavana07@users.noreply.github.com>
2024-10-29 15:51:50 +0530

    initial commit

--- app/src/main/java/com/example/nfcreader/MainActivity.java
+++ app/src/main/java/com/example/nfcreader/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,8 +21,8 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     private NfcAdapter nfcAdapter;
-
     private TextView nfcTextView;
+    private ImageView create_contact_IV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
             showNoNfcDialog();
             return;
         }
-        nfcTextView = findViewById(R.id.nfcTextView);
-        nfcTextView.setOnClickListener(new View.OnClickListener() {
+        create_contact_IV = findViewById(R.id.create_contact_IV);
+        create_contact_IV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AddContactActivity.class);
